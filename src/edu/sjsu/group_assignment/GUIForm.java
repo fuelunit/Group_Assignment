699688087c817bdeb3605ee8388b45d00b56c2f2
lilou <lilou.sicard-noel@sjsu.edu>
2021-07-29 11:22:47 -0400

    GUI Part2

--- src/edu/sjsu/group_assignment/GUIForm.java
+++ src/edu/sjsu/group_assignment/GUIForm.java
@@ -1,4 +1,22 @@
 package edu.sjsu.group_assignment;
 
+import com.github.lgooddatepicker.components.CalendarPanel;
+// Uses LGoodDatePicker from https://github.com/LGoodDatePicker/LGoodDatePicker/releases/tag/v11.2.1-Standard
+
+import javax.swing.*;
+
 public class GUIForm {
+
+    private JTextArea textArea1;
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.add(new CalendarPanel());
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
 }
